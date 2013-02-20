9495912536017bc2ea77e6061741ae6a6395e619
immars <immars@gmail.com>
2013-02-20 18:47:32 +0800

    more comment on Group

--- src/com/xingcloud/xa/session2/ra/Group.java
+++ src/com/xingcloud/xa/session2/ra/Group.java
@@ -8,6 +8,11 @@ import java.util.List;
  * Author: mulisen
  * Date:   2/7/13
  * Grouping 操作。
+ * 对于输入的Relation，
+ * 首先，根据groupingExpression 把所有行分成不同的组；
+ * 然后，每一组的最终输出一行，其所具有的列由projectionExpression决定。
+ * 按照标准的定义，projectionExpression可以包含两种表达式：
+ * 	groupingExpression中的表达式，以及对每一组中所有行的聚合表达式。
  */
 public interface Group extends Operation {
 	/**
